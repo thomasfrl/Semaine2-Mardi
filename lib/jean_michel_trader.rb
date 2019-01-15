69b204c7f6d8cfc0c4ee2ef21a0a24a1b56af62d
thomasfrl <feral.thomas@gmail.com>
2019-01-15 22:53:49 +0100

    trader fini

--- lib/jean_michel_trader.rb
+++ lib/jean_michel_trader.rb
@@ -1,24 +1,20 @@
 def day_trader(list_of_prices)
 	n = list_of_prices.size
 	hash_compare_price = Hash.new 
-	max_day = []
-	day_max = 0
-	list_of_prices.each_with_index do |price,i|
-		price_comparaison_of_day = 0
-		(n - i - 1).times do |j|
-			day_max = i
-			if price_comparaison_of_day < price - list_of_prices[i+j+1]
-				day_max = i+j+1
-				price_comparaison_of_day = price - list_of_prices[i+j+1]
+
+	list_of_prices.each_with_index do |price_of_buy_day,buy_day|
+		max_diff_for_buy_day = - Float::INFINITY
+		sell_day_max = buy_day
+		(n - buy_day-1).times do |j|
+			sell_day = buy_day + j + 1
+			if max_diff_for_buy_day < list_of_prices[sell_day] - price_of_buy_day
+				sell_day_max = sell_day
+				max_diff_for_buy_day = list_of_prices[sell_day] - price_of_buy_day
 			end
 		end	
-		hash_compare_price[day_max] = price_comparaison_of_day
-#		max_day[i] = price_comparaison_of_day.max
+		if buy_day != n-1
+			hash_compare_price[max_diff_for_buy_day] = [buy_day,sell_day_max]
+		end
 	end
-	max_total = hash_compare_price.max_by{ |k,v| v }[0]
+	return hash_compare_price.max_by{ |k,v| k }[1]
 end
-
-
-puts day_trader([17, 3, 6, 9, 15, 8, 6, 1, 10])
-
-#marche pas!!!!!!
